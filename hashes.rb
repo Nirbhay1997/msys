bdadb23755f7bfec1de487c29524b3b1284ff7f9
NIRBHAY KUMAR <42607997+Nirbhay1997@users.noreply.github.com>
2020-01-14 14:55:10 +0530

    Update hashes.rb

--- hashes.rb
+++ hashes.rb
@@ -8,3 +8,14 @@ state ={
 state.each_key{|k| puts "key : #{k} "}
 state.each_pair{|k,v| puts "key : #{k} value : #{v} "}
 state.each_value{|v| puts "value : #{v} "}
+
+state2 = state.invert
+
+puts state2.keys.inspect
+puts state2.values.inspect
+puts state.fetch(4)
+puts state.values_at 4,:bihar
+
+puts state.has_key?(:bihar)
+puts state.key? (4)
+puts state.include?("bengal")
